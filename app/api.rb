886a95789da65b7d3ca239e6d4fde68873570237
pradosh987 <pradosh987@gmail.com>
2015-07-15 17:33:53 +0530

    initial

---
+++ app/api.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+require 'json'
+
+class API
+	@@remote_api_end_point = 'https://api.themoviedb.org/3/'
+	@@api_key = 'b52469d21a984a24ec19edab6da3439e'
+
+	def self.prepare_query(query)
+		return @@remote_api_end_point + query + '?api_key=' + @@api_key
+	end
+
+	def self.call_api(url,parse_func = nil)
+		url = append_api_key(url)
+		resp = Net::HTTP.get_response(URI.parse(url))
+		return nil if resp.code != 200.to_s
+		response = JSON.parse(resp.body)
+	 	response = parse_func.call(response) if parse_func
+	end
+
+	#validate the response from api
+	#default_validator = lambda do
+
+end
+
+puts API.prepare_query('movie/118340')
+
+#https://api.themoviedb.org/3/movie/118340?api_key=b52469d21a984a24ec19edab6da3439e
+
