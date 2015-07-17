96c1596981f6b408cf06869db63bc1dc294394f6
pradosh987 <pradosh987@gmail.com>
2015-07-17 17:49:49 +0530

    changed the way default values were initialized

--- app/classes/Nav_Page.rb
+++ app/classes/Nav_Page.rb
@@ -1,7 +1,7 @@
 class Nav_Page
 	attr_accessor :page, :total_pages, :data 
 
-	def initialize(data, page: 1,total_pages: 1)
+	def initialize(data, page =1,total_pages =1)
 		@data = data
 		@page = page
 		@total_pages = total_pages
