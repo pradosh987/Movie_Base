0c1d631c85630d9c3f09bdda9092b12e8ee5351f
pradosh987 <pradosh987@gmail.com>
2015-07-17 17:49:07 +0530

    bug fix: title was and name were duplicate fields. Name is now removed

--- app/classes/Movie.rb
+++ app/classes/Movie.rb
@@ -1,10 +1,10 @@
 class Movie
 
-	attr_accessor :title, :name, :backdrop, :poster, :budget, :genre, :homepage, :id, :imdb_id, :language, :overview, :production_companies, :release_date, :runtime, :tagline, :ratings, :reviews
+	attr_accessor :title, :backdrop, :poster, :budget, :genre, :homepage, :id, :imdb_id, :language, :overview, :production_companies, :release_date, :runtime, :tagline, :ratings, :reviews
 
 	#initialize Movie. Name and id is mendatory
-  def initialize(name, id, opts={},reviews: {})
-    @name = name
+  def initialize(title, id, opts={},reviews: {})
+    @title = title
     @id = id
     opts.each_pair do |k ,v|
       self.send("#{k}=",v) if self.respond_to?("#{k}=")
